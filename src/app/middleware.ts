c72993109d0b5cb800dba7e59708a692eb565a58
L1nzh <596862470@qq.com>
2025-08-20 16:22:33 +0800

    test middleware

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,11 +1,19 @@
 // app/middleware.ts
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
   console.log('🟢 Middleware is WORKING! Path:', request.nextUrl.pathname);
-  return;
+
+  // 强制重定向，一看就知道生效了
+  if (request.nextUrl.pathname === '/admin') {
+    const url = request.nextUrl.clone();
+    url.pathname = '/admin/login'; // 强制跳转到登录页
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: '/admin/:path*',
+  matcher: ['/admin', '/admin/:path*'],
 };
